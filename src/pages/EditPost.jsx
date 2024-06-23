1162296b554365a2bad52de04bd3d19c09f830d5
Abhishek <abhishekkshirasahar2018@gmail.com>
2024-06-23 22:03:20 +0530

    bug fixed

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import { useNavigate,useParams } from "react-router-dom";
 import authservice from "../appwrite/config";
-import { Container, PostForm} from "../components"
+import { Container} from "../components"
 
 function EditPost(){
 
